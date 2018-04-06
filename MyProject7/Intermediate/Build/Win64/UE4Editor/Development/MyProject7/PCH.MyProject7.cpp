fb5a730e72d1dc2ed1b67e28e32cae8bf2ab3fbf
EiVinny <vinnyosantos@gmail.com>
2018-04-06 17:37:30 -0300

    Teste_1

---
+++ MyProject7/Intermediate/Build/Win64/UE4Editor/Development/MyProject7/PCH.MyProject7.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/vinny/Documents/Unreal Projects/MyProject7/Intermediate/Build/Win64/UE4Editor/Development/MyProject7/PCH.MyProject7.h"
---
+++ MyProject7/Source/MyProject7/MyProject7GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MyProject7GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT7_API AMyProject7GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
